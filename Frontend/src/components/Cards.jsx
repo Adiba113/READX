64720bdccdee9d5316becdf59014b9ea3561d4a1
srabanimitra <srabanimitra2222@gmail.com>
2024-06-13 20:04:09 +0600

    slide

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from 'react'
 
-export default function Cards({ item }) {
+export default function Cards({item}) {
   return (
+    <>
+    <div>
     <div className="card w-96 bg-base-100 shadow-xl">
-      <figure><img src={item.image} alt={item.title} /></figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          {item.title}
-          {item.isNew && <div className="badge badge-secondary">NEW</div>}
-        </h2>
-        <p>{item.description}</p>
-        <div className="card-actions justify-end">
-          {item.tags && item.tags.map((tag, index) => (
-            <div className="badge badge-outline" key={index}>{tag}</div>
-          ))}
-        </div>
-      </div>
+  <figure><img src={item.image} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Shoes!
+      <div className="badge badge-secondary">NEW</div>
+    </h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Fashion</div> 
+      <div className="badge badge-outline">Products</div>
     </div>
-  );
+  </div>
+</div>
+    </div>
+    </>
+  )
 }
