e3310692653c700383c1d8d2d4b0d4c0b45810bc
srabanimitra <srabanimitra2222@gmail.com>
2024-06-23 01:59:53 +0600

    login

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 //import React from 'react'
 import React, { useEffect } from 'react'
 import { useState } from 'react'
+import Login from './Login';
 
 function Navbar() {
 
@@ -58,7 +59,7 @@ function Navbar() {
       <div
         className={` max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-slate-800 dark:text-white fixed top-0 left-0 right-0 z-50 ${
           sticky
-            ? "sticky-navbar shadow-md bg-base-200 dark:bg-slate-700 dark:text-white duration-300 transition-all ease-in-out"
+            ? "sticky-navbar shadow-md bg-base-200 dark:bg-slate-600 dark:text-white duration-300 transition-all ease-in-out"
             : ""
         }`}
       >
@@ -92,7 +93,7 @@ function Navbar() {
             </div>
             <div className="hidden md:block">
               <label className="px-3 py-2 border-md flex items-center gap-2">
-                <input type="text" className="grow outline-none" placeholder="Search" />
+                <input type="text" className="grow outline-none px-3 dark:bg-slate-900 dark:text-white" placeholder="Search" />
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
               </label>
             </div>
@@ -119,8 +120,12 @@ function Navbar() {
 
             </label>
             <div className="navbar-end">
-              <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cousor-pointer">Login</a>
+              <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cousor-pointer"
+              onClick={()=>document.getElementById("my_modal_3").showModal()}
+              >Login</a>
+              <Login/>
             </div>
+           
           </div>
         </div>
       </div>
