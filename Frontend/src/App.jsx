6317880af0431f7463dfa2be36a5751a8ff4515f
srabanimitra <srabanimitra2222@gmail.com>
2024-07-12 21:54:36 +0600

    contact

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Courses from './courses/Courses'
 import Signup from './components/Signup'
 import   { Toaster } from 'react-hot-toast';
+import Contact from './components/Contact'
 
 
 
@@ -17,6 +18,7 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/course" element={<Courses />}/>
       <Route path="/signup" element={<Signup />}/>
+      <Route path="/contact" element={<Contact/>} />
     </Routes>
     <Toaster />
     </div>
