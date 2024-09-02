d2855c527e5c14d3672ea99aa95b106848461f81
Nusrat Jahan Adiba <nusratadiba06@gmail.com>
2024-09-02 21:12:03 +0600

    logout

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -12,22 +12,30 @@ function Login() {
 
   const onSubmit =async (data) => {
     const userInfo={
-      fullname:data.fullname,
-      email:data.email,
-      password:data.password,
-     }
-     await axios.post("http://localhost:4001/user/Signup",userInfo)
+      email: data.email,
+      password: data.password,
+     };
+     await axios
+     .post("http://localhost:4001/user/Login",userInfo)
      .then((res)=>{
       console.log(res.data)
       if(res.data){
       
         toast.success("logged in Successfully");
+        document.getElementById("my_modal_3").close();
+        setTimeout(() => {
+          window.location.reload();
+          localStorage.setItem("Users", JSON.stringify(res.data.user));
+        }, 1000);
       }
-     }).catch((err) =>{
+      
+     })
+     .catch((err) =>{
       if(err.response){
         console.log(err);
         
         toast.error("Error:" +err.response.data.message);
+        setTimeout(() => {}, 2000);
       }
      })
     }
@@ -37,10 +45,13 @@ function Login() {
     <div>
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
-          {/* Corrected form structure */}
-          <form onSubmit={handleSubmit(onSubmit)} dialog="true">
-            {/* Close button */}
-            <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+        <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <Link
+              to="/"
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
               ✕
             </Link>
             <h3 className="font-bold text-lg">Login</h3>
